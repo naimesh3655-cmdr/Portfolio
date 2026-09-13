4c3acae080370d63e960a5571b8916fef0846f57
naimesh3655-cmdr <naimesh3655@gmail.com>
2026-09-14 02:11:39 +0530

    Update settings.py

--- portfolio_django/portfolio/settings.py
+++ portfolio_django/portfolio/settings.py
@@ -10,7 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.1/ref/settings/
 """
 
+import os
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
